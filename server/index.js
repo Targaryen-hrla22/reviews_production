2fbe97cffa0b3c686b27ffb21401406e4076b002
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2018-06-16 14:00:40 -0700

    [add] CORS settings

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const express = require ('express');
 const parser = require('body-parser');
+const cors = require('cors');
 
 const { router } = require('./router');
 require('./../db');
 
 const PORT = 3000;
 const App = express();
+const corsOptions = {
+  origin: "https://amazonaws.com", 
+  optionsSuccessStatus: 200
+}
 
-
+App.use(cors(corsOptions));
 App.use(parser.json());
 App.use(parser.urlencoded({ extended: true }));
 
